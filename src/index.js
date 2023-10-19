a9d357cf680e6794d32d761476260d8bbcc9a072
ezequiel cabrera <ezelucabre@hotmail.com>
2023-10-19 16:16:57 -0300

    6

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TodoList from './components/TodoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
