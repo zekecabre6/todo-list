2b5f03c55096096c97dbd4947b8e7cc88af42b0e
ezequiel cabrera <ezelucabre@hotmail.com>
2023-10-19 16:41:39 -0300

    32423423423

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TodoList /> {/* Agrega el componente TodoList aquí */}
+        <TodoList />
       </header>
     </div>
   );
